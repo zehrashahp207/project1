25df821ec476d8e984176a75968dcda10d1cd6c3
zehrashahp207 <shahpelengovazehra@gmail.com>
2025-09-08 18:09:42 +0400

    task

--- registr/register.py
+++ registr/register.py
@@ -110,4 +110,4 @@ def logout():
     flash("Siz çıxış etdiniz.")
     return redirect(url_for("login"))
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0",port=1453)
